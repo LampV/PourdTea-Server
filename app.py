4671aec31e3043cae77e256e337d26106ce3cb46
LampV <1071952403@qq.com>
2019-11-03 00:10:49 +0800

    完成sample poem list 的代码

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import sqlite3
 import re
 
@@ -10,22 +10,25 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/poem/list/sample')
+@app.route('/poem/list/sample', methods=['POST'])
 def get_sample_poemlist():
+    data = request.get_json()
+    page = data['page']
     conn = sqlite3.connect('poem.db')
     c = conn.cursor()
 
-    columns = ['id', 'title', 'author', 'dynasty', 'abstract', 'type']
+    columns = ['id', 'title', 'author', 'dynasty', 'abstract', 'comment_count', 'type']
     sql = '''select _id, mingcheng, zuozhe, chaodai, zhaiyao, shipin, 'sample' from poem 
-    where chaodai='唐代' order by shipin desc limit 20'''
+    where chaodai='唐代' order by shipin desc limit 20 offset :offset'''
     assert (len(columns) == len(re.search(r'(?<=select).+(?=from)', sql).group(0).split(',')))
 
-    cursor = c.execute(sql)
+    cursor = c.execute(sql, {'offset': page * 20})
     result = []
     for r in cursor:
         result.append({
             columns[i]: r[i] for i in range(len(columns))
         })
+    return jsonify(result)
 
 
 if __name__ == '__main__':
